cea41642b473bcb371a09e7b229a1a05650feb8d
Viktor Burian <viktor@My-MacBook-Pro.local>
2022-12-31 17:06:25 +0100

    Comment dap, dap-ui

--- plugins/init.lua
+++ plugins/init.lua
@@ -12,70 +12,69 @@ return {
         }
       end,
     },
-    { "mfussenegger/nvim-dap",
-      config = function()
-        -- Debug configuration
-        local dap = require('dap')
-        dap.adapters.lldb = {
-          type = 'executable',
-          command = '/usr/local/opt/llvm/bin/lldb-vscode',
-          name = 'lldb'
-        }
-        dap.configurations.cpp = {
-          {
-            name = 'Launch',
-            type = 'lldb',
-            request = 'launch',
-            program = function()
-              return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-            end,
-            cwd = '${workspaceFolder}',
-            stopOnEntry = false,
-            args = {},
-          },
-        }
-        dap.configurations.c = dap.configurations.cpp
-        dap.configurations.rust = dap.configurations.cpp
-
-        -- DAP mappings:
-        local map = vim.api.nvim_set_keymap
-        map("n", "<f5>", ":lua require('dap').continue()<cr>", { desc = "Continue" })
-        map("n", "<f10>", ":lua require('dap').step_over()<cr>", { desc = "Step over" })
-        map("n", "<f11>", ":lua require('dap').step_into()<cr>", { desc = "Step into" })
-        map("n", "<f12>", ":lua require('dap').step_out()<cr>", { desc = "Step out" })
-        map("n", "<leader>db", ":lua require('dap').toggle_breakpoint()<cr>", { desc = "Toggle breakpoint" })
-        map("n", "<leader>dx", ":lua require('dap').clear_breakpoints()<cr>", { desc = "Clear breakpoints" })
-        map(
-          "n",
-          "<leader>dc",
-          ":lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))<cr>",
-          { desc = "Set conditional breakpoint" }
-        )
-        map(
-          "n",
-          "<leader>dl",
-          ":lua require('dap').set_breakpoint(nil, nil, vim.fn.input('Logpoint message: '))<cr>",
-          { desc = "Set logpoint" }
-        )
-        map("n", "<leader>dr", ":lua require('dap').repl.open()<cr>", { desc = "Open REPL" })
-        map("n", "<leader>dR", ":lua require('dap').run_last()<cr>", { desc = "Run last debugged program" })
-        map("n", "<leader>dp", ":lua require('dap.utils').pick_process()<cr>", { desc = "Attach to process" })
-        map("n", "<leader>dX", ":lua require('dap').terminate()<cr>", { desc = "Terminate program being debugged" })
-        map("n", "<leader>du", ":lua require('dap').up()<cr>", { desc = "Up one frame" })
-        map("n", "<leader>dd", ":lua require('dap').down()<cr>", { desc = "Down one frame" })
-        map("n", "<leader>de", ":lua require('dapui').eval()<cr>", { desc = "Evaluate expression" })
-      end},
-    { "rcarriga/nvim-dap-ui",
-      requires = { "nvim-dap", "rust-tools.nvim" },
-      config = function()
-        local dapui = require "dapui"
-        dapui.setup {icons = { expanded = "", collapsed = "▸", current_frame = "▸" },}
-
-        local dap = require "dap"
-        dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
-        dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
-      end,
-    },
+    -- { "mfussenegger/nvim-dap",
+    --   config = function()
+    --     -- Debug configuration
+    --     local dap = require('dap')
+    --     dap.adapters.lldb = {
+    --       type = 'executable',
+    --       command = '/usr/local/opt/llvm/bin/lldb-vscode',
+    --       name = 'lldb'
+    --     }
+    --     dap.configurations.cpp = {
+    --       {
+    --         name = 'Launch',
+    --         type = 'lldb',
+    --         request = 'launch',
+    --         program = function()
+    --           return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    --         end,
+    --         cwd = '${workspaceFolder}',
+    --         stopOnEntry = false,
+    --         args = {},
+    --       },
+    --     }
+    --     dap.configurations.c = dap.configurations.cpp
+    --     dap.configurations.rust = dap.configurations.cpp
+    --
+    --     -- DAP mappings:
+    --     local map = vim.api.nvim_set_keymap
+    --     map("n", "<f5>", ":lua require('dap').continue()<cr>", { desc = "Continue" })
+    --     map("n", "<f10>", ":lua require('dap').step_over()<cr>", { desc = "Step over" })
+    --     map("n", "<f11>", ":lua require('dap').step_into()<cr>", { desc = "Step into" })
+    --     map("n", "<leader>db", ":lua require('dap').toggle_breakpoint()<cr>", { desc = "Toggle breakpoint" })
+    --     map("n", "<leader>dx", ":lua require('dap').clear_breakpoints()<cr>", { desc = "Clear breakpoints" })
+    --     map(
+    --       "n",
+    --       "<leader>dc",
+    --       ":lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))<cr>",
+    --       { desc = "Set conditional breakpoint" }
+    --     )
+    --     map(
+    --       "n",
+    --       "<leader>dl",
+    --       ":lua require('dap').set_breakpoint(nil, nil, vim.fn.input('Logpoint message: '))<cr>",
+    --       { desc = "Set logpoint" }
+    --     )
+    --     map("n", "<leader>dr", ":lua require('dap').repl.open()<cr>", { desc = "Open REPL" })
+    --     map("n", "<leader>dR", ":lua require('dap').run_last()<cr>", { desc = "Run last debugged program" })
+    --     map("n", "<leader>dp", ":lua require('dap.utils').pick_process()<cr>", { desc = "Attach to process" })
+    --     map("n", "<leader>dX", ":lua require('dap').terminate()<cr>", { desc = "Terminate program being debugged" })
+    --     map("n", "<leader>du", ":lua require('dap').up()<cr>", { desc = "Up one frame" })
+    --     map("n", "<leader>dd", ":lua require('dap').down()<cr>", { desc = "Down one frame" })
+    --     map("n", "<leader>de", ":lua require('dapui').eval()<cr>", { desc = "Evaluate expression" })
+    --   end},
+    -- { "rcarriga/nvim-dap-ui",
+    --   requires = { "nvim-dap", "rust-tools.nvim" },
+    --   config = function()
+    --     local dapui = require "dapui"
+    --     dapui.setup {icons = { expanded = "", collapsed = "▸", current_frame = "▸" },}
+    --
+    --     local dap = require "dap"
+    --     dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
+    --     dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
+    --   end,
+    -- },
     {
         "mfussenegger/nvim-dap-python",
         config = function ()

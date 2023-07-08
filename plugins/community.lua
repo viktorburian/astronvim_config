5243c9de04138ccf3af6b08aa10587153ac1dad2
Viktor Burian <viktor.burian@macbookpro>
2023-07-08 20:12:18 +0200

    Add nvim surround

--- plugins/community.lua
+++ plugins/community.lua
@@ -2,4 +2,5 @@ return {
     "AstroNvim/astrocommunity",
     { import = "astrocommunity.colorscheme.catppuccin" },
     { import = "astrocommunity.scrolling.mini-animate" },
+    { import = "astrocommunity.motion.nvim-surround" },
 }
